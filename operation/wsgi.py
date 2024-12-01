745a48723b319a957d39cb55a092646ee25d92ff
hwoldeamanuel <habtamuwh@gmail.com>
2024-12-01 22:48:31 +0300

    collectstatic

--- operation/wsgi.py
+++ operation/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'operation.settings')
+settings_module = 'operation.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'operation.settings'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
